fda07dba39feb090ab6e8a1a3f508010a75f1521
Tobias <pito.mailing@gmail.com>
2025-09-05 23:56:29 +0200

    Add flags for local cuda wheel builds and uploading wheels to PyPI

--- local_cibuildwheel.sh
+++ local_cibuildwheel.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+if [[ $* == *--cuda* ]]
+then
+    export BUILD_CUDA_ENABLED="ON"
+else
+    export BUILD_CUDA_ENABLED="OFF"
+fi
+
 export VCPKG_TARGET_TRIPLET="x64-linux-release"
 
 
@@ -27,7 +34,7 @@ export CCACHE_BASEDIR="/project"
 export CIBW_BUILD="cp3{8,9,10,11,12,13}-manylinux*"
 
 # Make sure environment variables are passed to the container by cibuildwheel
-export CIBW_ENVIRONMENT_PASS_LINUX="VCPKG_TARGET_TRIPLET VCPKG_INSTALLATION_ROOT VCPKG_DEFAULT_BINARY_CACHE CMAKE_TOOLCHAIN_FILE VCPKG_BINARY_SOURCES CONTAINER_COMPILER_CACHE_DIR CCACHE_DIR CCACHE_BASEDIR"
+export CIBW_ENVIRONMENT_PASS_LINUX="VCPKG_TARGET_TRIPLET VCPKG_INSTALLATION_ROOT VCPKG_DEFAULT_BINARY_CACHE CMAKE_TOOLCHAIN_FILE VCPKG_BINARY_SOURCES CONTAINER_COMPILER_CACHE_DIR CCACHE_DIR CCACHE_BASEDIR BUILD_CUDA_ENABLED"
 
 # Use a CUDA-enabled manylinux container image
 export CIBW_MANYLINUX_X86_64_IMAGE="sameli/manylinux_2_34_x86_64_cuda_12.8"
@@ -38,4 +45,12 @@ export CIBW_REPAIR_WHEEL_COMMAND="auditwheel repair --exclude libcudart* --exclu
 # Uncomment the following line to not delete the container after the build. Really helpful for debugging purposes!
 #export CIBW_DEBUG_KEEP_CONTAINER=True
 
+cd "${SCRIPT_DIR}"
+
 uvx cibuildwheel --platform=linux
+
+if [[ $* == *--upload* ]]
+then
+    echo "Uploading wheels to PyPI"
+    uvx twine upload wheelhouse/*
+fi
